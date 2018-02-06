9b7445c98f76e4878f8eefff36d3437cd8c55ee0
xiaoming <834557263@qq.com>
2018-02-06 10:03:50 +0800

    before merge

---
+++ src/components/main-menu-item.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: '',
+        props: [],
+        components: {},
+        data() {
+            return {}
+        },
+        computed: {},
+        methods: {},
+        created() {
+        },
+        mounted() {
+        }
+    }
+</script>
+<style lang="less" scoped>
+</style>
